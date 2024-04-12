2553f21522864bc87055be6c6fa1402bb8e9ccae
Frank Davies <fdavies93@gmail.com>
2024-04-12 13:46:30 +0800

    Got basic indexing and path walking working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 use clap::Parser;
+use std::collections::VecDeque;
 use std::path::Path;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 struct Args {
     path: String,
 }
 
-fn walk(path: &Path) {
-    for entry in path.read_dir().expect("Couldn't read the directory.") {}
+fn walk(path: &Path) -> Vec<PathBuf> {
+    let mut vec: Vec<PathBuf> = Vec::new();
+
+    // bfs for directories
+    let mut paths = VecDeque::from(vec![path.to_path_buf()]);
+    loop {
+        if paths.len() == 0 {
+            break;
+        }
+        let current_path = paths.pop_front().expect("");
+
+        // might want to check with pathbufs whether this causes a memory leak due to copies
+
+        for entry in current_path.read_dir().expect("read_dir failed") {
+            if let Ok(entry) = entry {
+                let pathbuf = entry.path();
+                let entry_path = pathbuf.as_path();
+                if entry_path.is_file() && entry_path.extension().expect("") == "md" {
+                    vec.push(pathbuf);
+                } else if entry_path.is_dir() {
+                    paths.push_back(entry_path.to_path_buf());
+                }
+                // no symlink support for now
+            }
+        }
+    }
+
+    return vec;
 }
 
 fn main() {
@@ -15,18 +43,20 @@ fn main() {
 
     let path = Path::new(&args.path);
 
-    if !path.try_exists().is_ok_and(|x| x) {:qa
+    if !path.try_exists().is_ok_and(|x| x) {
         println!("Couldn't access path at {}", args.path);
         return;
     }
 
-    let mut vec: Vec<&Path> = Vec::new();
+    let mut vec: Vec<PathBuf> = Vec::new();
 
     if path.is_file() && path.extension().unwrap() == "md" {
-        vec.push(path)
+        vec.push(path.to_path_buf())
     } else if path.is_dir() {
         // crawl the directory structure
+        let paths = walk(path);
+        vec.extend(paths.into_iter());
     }
 
-    println!("{}", args.path);
+    println!("{:?}", vec);
 }
