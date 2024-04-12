f49bb915d1718894fbdffef6fda328e4f274c651
Frank Davies <fdavies93@gmail.com>
2024-04-12 18:39:31 +0800

    Got async parsing running successfully

--- src/zenkat/main.rs
+++ src/zenkat/main.rs
@@ -1,8 +1,8 @@
-use async_process::Command;
 use clap::Parser;
 use std::collections::VecDeque;
 use std::path::Path;
 use std::path::PathBuf;
+use tokio::process::Command;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -45,28 +45,42 @@ fn walk(path: &Path, follow_symlinks: bool) -> Vec<PathBuf> {
             }
         }
     }
+    println!("{}", vec.len());
 
     return vec;
 }
 
-fn parse_at_paths(paths: Vec<PathBuf>, processes: u8) {
+async fn parse_at_paths(paths: Vec<PathBuf>, processes: usize, parser: &str) -> Vec<String> {
     let mut parsed: Vec<String> = vec![];
-    let mut commands: Vec<async_process::Child> = vec![];
+    let mut children: VecDeque<_> = VecDeque::new();
+    let mut remaining_paths: Vec<PathBuf> = paths.to_vec();
 
     loop {
-        if paths.len() == 0 && commands.len() == 0 { break }
-        if commands.len() < processes &&  {}
+        if remaining_paths.len() == 0 && children.len() == 0 {
+            break;
+        }
+        loop {
+            if children.len() == processes || remaining_paths.len() == 0 {
+                break;
+            }
+            let next_path = remaining_paths.pop().expect("");
+            let child = Command::new(parser).arg(next_path).output();
+            children.push_back(child);
+        }
+        let output = children.pop_front().expect("").await.expect("");
+        let string = String::from_utf8(output.stdout).expect("");
+        parsed.push(string);
     }
-    for path in paths {
-        let child = Command::new("zk-parse").arg("path").spawn().expect("");
-        commands.push(child);
-    }
+    return parsed;
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
     let path = Path::new(&args.path);
+
+    let parser = "target/debug/zk-parse";
 
     if !path.try_exists().is_ok_and(|x| x) {
         println!("Couldn't access path at {}", args.path);
@@ -75,6 +89,9 @@ fn main() {
 
     let mut vec: Vec<PathBuf> = Vec::new();
 
+    let pathbuf = path.canonicalize().expect("");
+    let path = pathbuf.as_path();
+    println!("{:?}", path);
     if path.is_file() && path.extension().unwrap() == "md" {
         vec.push(path.to_path_buf())
     } else if path.is_dir() {
@@ -89,5 +106,6 @@ fn main() {
         &args.path
     );
 
-    parse_at_paths(vec, 4);
+    let parsed = parse_at_paths(vec, 4, parser).await;
+    println!("{:?}", parsed);
 }
