8cff049369c94aaf7e0a894a3d10df46d3e5b0be
Frank Davies <fdavies93@gmail.com>
2024-04-13 16:56:13 +0800

    Add handler for ATX headers

--- src/md-parse/main.rs
+++ src/md-parse/main.rs
@@ -14,6 +14,7 @@ struct Args {
 pub enum NodeType {
     DOCUMENT,
     PARAGRAPH,
+    HEADER,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -33,6 +34,68 @@ impl Node {
             data: HashMap::new(),
         }
     }
+}
+
+fn parse_atx_header(raw: String) -> Option<(String, Node)> {
+    let mut unconsumed = raw;
+    let mut buffer = String::new();
+
+    // parse leading # signs
+    loop {
+        if unconsumed.is_empty() {
+            break;
+        }
+
+        let (prefix, suffix) = unconsumed.split_at(1);
+
+        if prefix == "#" {
+            buffer.push_str(prefix);
+            unconsumed = String::from(suffix);
+        } else if prefix == " " {
+            unconsumed = String::from(suffix);
+            break;
+        } else {
+            // #abc is not a valid ATX header; it's a tag
+            return None;
+        }
+    }
+
+    if buffer.len() < 1 {
+        return None;
+    }
+
+    let rank = buffer.len();
+
+    buffer.clear();
+
+    // could add a section here for handling whitespace between ATX tag and header text
+
+    // parse rest of line
+    loop {
+        if unconsumed.is_empty() {
+            break;
+        }
+
+        let (prefix, suffix) = unconsumed.split_at(1);
+
+        if prefix == "\n" {
+            break;
+        }
+
+        buffer.push_str(prefix);
+        unconsumed = String::from(suffix);
+    }
+
+    // ## is not a valid header; it's empty
+    if buffer.len() == 0 {
+        return None;
+    }
+
+    let mut node = Node::new(buffer, NodeType::HEADER);
+
+    node.data.insert(String::from("rank"), rank.to_string());
+
+    return Some((unconsumed, node));
 }
 
 fn parse_paragraph(raw: String) -> Option<(String, Node)> {
@@ -64,6 +127,8 @@ fn parse_paragraph(raw: String) -> Option<(String, Node)> {
 }
 
 fn parse_document(path: &str) -> Node {
+    let parse_fns: Vec<&dyn Fn(String) -> Option<(String, Node)>> =
+        vec![&parse_atx_header, &parse_paragraph];
     let raw = read_to_string(path).expect("");
     let mut output = Node::new(raw.clone(), NodeType::DOCUMENT);
     output.data.insert(String::from("path"), String::from(path));
@@ -74,11 +139,15 @@ fn parse_document(path: &str) -> Node {
         if to_parse.is_empty() {
             break;
         }
-        let result = parse_paragraph(to_parse.clone());
-        if result.is_some() {
-            let (remaining, node) = result.unwrap();
-            to_parse = remaining;
-            output.blocks.push(node);
+
+        for f in &parse_fns {
+            let result = f(to_parse.clone());
+
+            if result.is_some() {
+                let (remaining, node) = result.unwrap();
+                to_parse = remaining;
+                output.blocks.push(node);
+            }
         }
     }
 
