5c7de8a6bf8ce2f7f335f9de1fd066f02ae6da4d
Frank Davies <fdavies93@gmail.com>
2024-08-30 00:15:16 +0800

    Implement blank line and paragraph parser with Nom

--- src/md-parse/main.rs
+++ src/md-parse/main.rs
@@ -1,11 +1,14 @@
 use clap::Parser;
 use nom::branch::alt;
-use nom::bytes::complete::{is_a, take_until};
-use nom::character::complete::char;
-use nom::combinator::opt;
-use nom::multi::many0;
-use nom::sequence::tuple;
+use nom::bytes::complete::{is_a, tag, take, take_till, take_until};
+use nom::character::complete::{
+    alphanumeric0, anychar, char, line_ending, not_line_ending, space0,
+};
+use nom::combinator::{eof, not, opt, rest};
+use nom::multi::{fold_many0, many0, many1, many_till};
+use nom::sequence::{terminated, tuple};
 use serde_json::to_string;
+use std::fs::{self, read_to_string};
 use std::io::{self, Write};
 
 #[path = "../common.rs"]
@@ -19,36 +22,80 @@ struct Args {
     path: String,
 }
 
-fn paragraph(raw: &str) -> IResult<&str, Node> {
+fn blank_line(raw: &str) -> IResult<&str, Tree> {
+    let result = tuple((
+        line_ending,
+        terminated(
+            space0,
+            line_ending,
+        ),
+    ))(raw);
+    match result {
+        Ok((stream, results)) => {
+            let node = Node::new(NodeType::None);
+            let tree = Tree::new(node);
+            return Ok((
+                stream, tree,
+            ));
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    }
+}
+
+fn paragraph(raw: &str) -> IResult<&str, Tree> {
     // This is a decent example for transforming a character specification into
     // a unit of meaning
-    let result = tuple((opt(char('\n')), take_until("\n\n")))(raw);
+    // Might need an indent level modifier
+    let result = many_till(
+        anychar, blank_line,
+    )(raw);
     match result {
         Ok((stream, results)) => {
             let mut node = Node::new(NodeType::PARAGRAPH);
-            let content = results.1;
-            node.data = NodeData::ParagraphData {
-                text: content.into(),
-            };
-            return Ok((stream, node));
+            let content: String = String::from_iter(results.0);
+            node.data = NodeData::ParagraphData { text: content };
+            return Ok((
+                stream,
+                Tree::new(node),
+            ));
         }
         Err(e) => return Err(e),
     }
 }
 
-fn block(raw: &str) -> IResult<&str, Node> {
-    return alt((paragraph,))(raw);
+fn block(raw: &str) -> IResult<&str, Tree> {
+    return alt((
+        blank_line, paragraph,
+    ))(raw);
 }
 
-fn document(path: &String) -> Tree {
-    many0(block)(path);
-    return Tree::new(Node::new(NodeType::DOCUMENT));
+fn document(raw: &str) -> IResult<&str, Tree> {
+    let blocks = many0(block)(raw);
+    match blocks {
+        Ok((stream, results)) => {
+            let root = Node::new(NodeType::DOCUMENT);
+            let mut tree = Tree::new(root);
+            for block in results {
+                tree.insert_child_under(
+                    block,
+                    tree.root_node.clone(),
+                );
+            }
+            return Ok((
+                stream, tree,
+            ));
+        }
+        Err(e) => return Err(e),
+    }
 }
 
 fn main() {
     let args = Args::parse();
 
-    let tree = document(&args.path);
+    let str = read_to_string(args.path).unwrap();
+    let tree = document(str.as_str()).unwrap();
 
     let json = to_string(&tree).expect("");
 
